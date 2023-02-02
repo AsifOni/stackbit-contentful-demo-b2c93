4ac9180a46b076dfc868871d213f298c58a950a3
Stackbit Code Editor <projects@stackbit.com>
2023-02-02 22:50:25 +0000

    [[...slug]].jsx: changed

--- pages/[[...slug]].jsx
+++ pages/[[...slug]].jsx
@@ -19,7 +19,7 @@ const componentMap = {
     hero: Hero,
     stats: Stats,
     button: Button,
-    
+    ThreeColumnCard: ThreeColumnCard
 };
 
 export default function ComposablePage({ page }) {
