5190fe1967a4c145c403d7df62c1df63675513b3
Stackbit Code Editor <projects@stackbit.com>
2023-02-03 01:01:10 +0000

    ThreeColumnCard.jsx: changed

--- components/ThreeColumnCard.jsx
+++ components/ThreeColumnCard.jsx
@@ -5,7 +5,7 @@ export const ThreeColumnCard = (props) => {
     const { title, caption, columnData } = props;
 
     return (
-        <div className="md:container mx-auto pad">
+        <div className="md:container mx-auto pad" data-sb-object-id={props.id}>
             {title && <h2 className="text-center text-blue-charcoal text-xl font-bold mb-8">{title}</h2>}
 
             {caption && <p className="leading-normal text-abbey mb-4">{caption}</p>}
