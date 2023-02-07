3389e970667eddd81bceb838a98acf6a7d6ff9bf
Stackbit Code Editor <projects@stackbit.com>
2023-02-07 22:03:44 +0000

    content.js: changed

--- utils/content.js
+++ utils/content.js
@@ -30,6 +30,7 @@ export async function getPageFromSlug(slug) {
         page = (items ?? [])[0];
     }
     if (!page) throw new Error(`Page not found for slug: ${slug}`);
+    const object = mapEntry(page);
     console.log()
     return mapEntry(page);
 }
