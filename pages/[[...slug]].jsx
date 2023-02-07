d221256d2fc4ab9750cb82e81bc8c42c972c439e
Stackbit Code Editor <projects@stackbit.com>
2023-02-07 22:00:01 +0000

    [[...slug]].jsx: changed

--- pages/[[...slug]].jsx
+++ pages/[[...slug]].jsx
@@ -12,7 +12,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     const slug = '/' + (params?.slug ?? ['']).join('/');
     const page = await getPageFromSlug(slug);
-    console.log('Content type', content_type);
+    console.log('Content type', page);
     return { props: { page } };
 }
 
