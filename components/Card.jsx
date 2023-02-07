c593469f5a0464f560a736b1238d79a9cadafe70
Stackbit Code Editor <projects@stackbit.com>
2023-02-07 20:40:17 +0000

    Card.jsx: changed

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,62 +6,62 @@ import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 import { CustomImage } from './CustomImage.jsx';
 
 export const Card = (props) => {
-    const themeClassMap = {
-        container: {
-            left: 'items-left',
-            center: 'items-center',
-        },
-        typoGraphy: {
-            left: 'text-left',
-            center: 'text-center',
-        },
-    };
-    const { imageMeta, title, subCopy, columnLink, theme = 'left' } = props;
+  const themeClassMap = {
+    container: {
+      left: 'items-left',
+      center: 'items-center',
+    },
+    typoGraphy: {
+      left: 'text-left',
+      center: 'text-center',
+    },
+  };
+  const { imageMeta, title, subCopy, columnLink, theme = 'left' } = props;
 
-    const options = {
-        renderNode: {
-            [BLOCKS.LIST_ITEM]: (node, children) => (
-                <li key={`${node.content[0].content[0].value}'-key'`} className="text-base">
-                    {node.content[0].content[0].value}
-                </li>
-            ),
-            [BLOCKS.UL_LIST]: (node, children) => <ul className="list-disc ml-5">{children}</ul>,
-            [INLINES.HYPERLINK]: (node, children) => (
-                <Link href={columnLink}>
-                    <a href={node.data.uri}>{children[0]}</a>
-                </Link>
-            ),
-            [BLOCKS.PARAGRAPH]: (node, children) => (
-                <p key={`${children}'-key'`} className={`leading-normal text-abbey ${themeClassMap.typoGraphy[theme]}`}>
-                    {children}
-                </p>
-            ),
-        },
-    };
+  const options = {
+    renderNode: {
+      [BLOCKS.LIST_ITEM]: (node, children) => (
+        <li key={`${node.content[0].content[0].value}'-key'`} className="text-base">
+          {node.content[0].content[0].value}
+        </li>
+      ),
+      [BLOCKS.UL_LIST]: (node, children) => <ul className="list-disc ml-5">{children}</ul>,
+      [INLINES.HYPERLINK]: (node, children) => (
+        <Link href={node.data.uri}>
+          <a rel="noreferrer">{children[0]}</a>
+        </Link>
+      ),
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <p key={`${children}'-key'`} className={`leading-normal text-abbey ${themeClassMap.typoGraphy[theme]}`}>
+          {children}
+        </p>
+      ),
+    },
+  };
 
-    const CardBody = () => {
-        return (
-            <div className={`flex flex-col ${themeClassMap.container[theme]}`}>
-                <div className={`mt-2 mb-4`}>
-                    <CustomImage theme={columnLink ? 'rounded-xl' : ''} {...imageMeta} />
-                </div>
-                <h3 className="mb-3 text-blue-charcoal font-bold">{title}</h3>
-                {subCopy && documentToReactComponents(subCopy, options)}
-            </div>
-        );
-    };
-
+  const CardBody = () => {
     return (
-        <>
-            {columnLink ? (
-                <Link href={columnLink}>
-                    <a>
-                        <CardBody />
-                    </a>
-                </Link>
-            ) : (
-                    <CardBody />
-                )}
-        </>
+      <div className={`flex flex-col ${themeClassMap.container[theme]}`}>
+        <div className={`mt-2 mb-4`}>
+          <CustomImage theme={columnLink ? 'rounded-xl' : ''} {...imageMeta} />
+        </div>
+        <h3 className="mb-3 text-blue-charcoal font-bold">{title}</h3>
+        {subCopy && documentToReactComponents(subCopy, options)}
+      </div>
     );
+  };
+
+  return (
+    <>
+      {columnLink ? (
+        <Link href={columnLink}>
+          <a rel="noreferrer">
+            <CardBody />
+          </a>
+        </Link>
+      ) : (
+        <CardBody />
+      )}
+    </>
+  );
 };
