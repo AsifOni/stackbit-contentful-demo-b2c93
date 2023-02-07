85d03f41eb4eb416f2b8ef0ed8aeb726cf03e8fc
Stackbit Code Editor <projects@stackbit.com>
2023-02-07 20:57:29 +0000

    content.js: changed

--- utils/content.js
+++ utils/content.js
@@ -4,7 +4,7 @@ const PAGE_CONTENT_TYPE_ID = 'page';
 const IS_DEV = process.env.NODE_ENV === 'development';
 
 async function getEntries(content_type, queryParams) {
-    console.log()
+    console.log('Content type')
   const client = createClient({
     accessToken: IS_DEV ? process.env.CONTENTFUL_PREVIEW_TOKEN : process.env.CONTENTFUL_DELIVERY_TOKEN,
     space: process.env.CONTENTFUL_SPACE_ID,
