e57d2adbcd21db292d057f872b9bfe3476ae4dce
Stackbit Code Editor <projects@stackbit.com>
2023-02-07 17:59:07 +0000

    ThreeColumnCard.jsx: changed

--- components/ThreeColumnCard.jsx
+++ components/ThreeColumnCard.jsx
@@ -16,6 +16,7 @@ export const ThreeColumnCard = (props) => {
 
                     return (
                         <>
+                            
                             <Card
                                 imageMeta={image}
                                 columnLink={columnLink}
