56248c7f5e784fd6025b120dc62ce549b7a14b6c
Stackbit Code Editor <projects@stackbit.com>
2023-02-23 19:20:22 +0000

    Card.jsx: changed

--- components/Card.jsx
+++ components/Card.jsx
@@ -47,7 +47,7 @@ export const Card = (props) => {
                     <CustomImage theme={columnLink ? 'rounded-xl' : ''} {...imageMeta} />
                 </div>
                 <h3 className="mb-3 text-blue-charcoal font-bold">{title}</h3>
-                <subCopy && (Markdown className="mb-6 text-lg">
+                { subCopy && (Markdown className="mb-6 text-lg">
                     {subCopy}
                 </Markdown>)
                 {/* {subCopy && documentToReactComponents(subCopy, options)} */}
