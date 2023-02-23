ea286e115bff4443d069997031c91ecb6b375dc4
Stackbit Code Editor <projects@stackbit.com>
2023-02-23 19:26:32 +0000

    Card.jsx: changed

--- components/Card.jsx
+++ components/Card.jsx
@@ -40,22 +40,22 @@ export const Card = (props) => {
     //     },
     // };
 
-    const CardBody = () => {
-        return (
-            <div className={`flex flex-col ${themeClassMap.container[theme]}`}>
-                <div className={`mt-2 mb-4`}>
-                    <CustomImage theme={columnLink ? 'rounded-xl' : ''} {...imageMeta} />
-                </div>
-                <h3 className="mb-3 text-blue-charcoal font-bold">{title}</h3>
-                {subCopy &&
-                    (<Markdown options={{ forceBlock: true }} className="mb-6 text-lg">
-                        {subCopy}
-                    </Markdown>)
-                }
-                {/* {subCopy && documentToReactComponents(subCopy, options)} */}
-            </div>
-        );
-    };
+    // const CardBody = () => {
+    //     return (
+    //         <div className={`flex flex-col ${themeClassMap.container[theme]}`}>
+    //             <div className={`mt-2 mb-4`}>
+    //                 <CustomImage theme={columnLink ? 'rounded-xl' : ''} {...imageMeta} />
+    //             </div>
+    //             <h3 className="mb-3 text-blue-charcoal font-bold">{title}</h3>
+    //             {subCopy &&
+    //                 (<Markdown options={{ forceBlock: true }} className="mb-6 text-lg">
+    //                     {subCopy}
+    //                 </Markdown>)
+    //             }
+    //             {/* {subCopy && documentToReactComponents(subCopy, options)} */}
+    //         </div>
+    //     );
+    // };
 
     return (
         <>
