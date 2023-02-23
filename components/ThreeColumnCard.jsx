4bd7209708abefdc0519728521c3af61d78983a2
Stackbit Code Editor <projects@stackbit.com>
2023-02-23 19:34:11 +0000

    ThreeColumnCard.jsx: changed

--- components/ThreeColumnCard.jsx
+++ components/ThreeColumnCard.jsx
@@ -8,7 +8,7 @@ export const ThreeColumnCard = (props) => {
         <div className="md:container mx-auto pad my-8" data-sb-object-id={props.id}>
             {title && <h2 className="text-center text-blue-charcoal text-xl font-bold mb-8">{title}</h2>}
 
-            {caption && <p className="leading-normal text-abbey mb-4">{caption}</p>}
+            {caption && <p className="leading-normal text-abbey mb-4 text-center">{caption}</p>}
 
             <div className="flex flex-row flex-wrap md:flex-nowrap md:space-x-5">
                 {columnData?.map((column, idx) => {
