fb3c338ac6f078a9c178562ca27ea444896150e2
Stackbit Code Editor <projects@stackbit.com>
2023-02-23 19:21:47 +0000

    Card.jsx: changed

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,3 +1,4 @@
+import Markdown from 'markdown-to-jsx';
 import Link from 'next/link';
 // import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 // import { BLOCKS, INLINES } from '@contentful/rich-text-types';
