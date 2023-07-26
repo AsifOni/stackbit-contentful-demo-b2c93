74b129815e7dbd0f474e3c42d1fe020d51c5e5e7
AsifOni <asif.oni@astralforminc.com>
2023-07-25 21:17:20 -0400

    Adding rewards button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
+import { Button as RewardsButton } from 'ccg-rewards';
 
 const themeClassMap = {
-    default: 'border-violet-800 bg-violet-800 text-white hover:bg-violet-600 hover:border-violet-600',
-    outline: 'border-black bg-transparent text-black hover:text-gray-500 hover:border-gray-500',
+  default: 'border-violet-800 bg-violet-800 text-white hover:bg-violet-600 hover:border-violet-600',
+  outline: 'border-black bg-transparent text-black hover:text-gray-500 hover:border-gray-500',
 };
 
 export const Button = (props) => {
-    return (
-        <Link href={props.url} passHref legacyBehavior>
+  return (
+    <>
+      {/* <Link href={props.url} passHref legacyBehavior>
             <a
                 rel="noreferrer"
                 className={`py-3 px-8 inline-block border rounded-md transition-all duration-300 ${
@@ -16,6 +18,11 @@ export const Button = (props) => {
             >
                 {props.label}
             </a>
-        </Link>
-    );
+        </Link> */}
+      <RewardsButton
+        variant="primary"
+        label={props.label}        
+      />
+    </>
+  );
 };
